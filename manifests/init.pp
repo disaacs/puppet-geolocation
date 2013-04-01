be0414b44e773adc0f807e3e22aea320b7947818
Burke Libbey <burke@libbey.me>
2013-04-01 13:05:28 -0500

    update command to not fail on existence

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 class geolitecity {
   exec { "download database":
-    command => "curl http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz -o /tmp/GeoLiteCity.dat.gz && gunzip /tmp/GeoLiteCity.dat.gz && sudo mkdir -p  /usr/share/GeoIP/ && sudo mv -n /tmp/GeoLiteCity.dat /usr/share/GeoIP/GeoIPCity.dat"
+    command => "sudo sh -c 'mkdir -p /usr/share/GeoIP && curl http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz | gunzip > /usr/share/GeoIP/GeoIPCity.dat'"
   }
 }
