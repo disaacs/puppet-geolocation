3ec17ab74ad86841e48781948265235584c5cbe3
Burke Libbey <burke@libbey.me>
2013-04-02 12:59:21 -0500

    onlyif to prevent output when it doesn't need to run

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,8 @@
 class geolitecity {
   exec { "download database":
-    command => "sudo sh -c 'test -s /usr/share/GeoIP/GeoIPCity.dat || mkdir -p /usr/share/GeoIP && curl http://s3.amazonaws.com/burkelibbey/GeoIPCity.dat.gz | gunzip > /usr/share/GeoIP/GeoIPCity.dat'"
+    command => "sudo sh -c 'mkdir -p /usr/share/GeoIP && curl http://s3.amazonaws.com/burkelibbey/GeoIPCity.dat.gz | gunzip > /usr/share/GeoIP/GeoIPCity.dat'",
+    onlyif => [
+      "test ! -s /usr/share/GeoIP/GeoIPCity.dat"
+    ]
   }
 }
