449d5b15042a64247ad295f5abc7df4ffedf794b
Dave Isaacs <dave.isaacs@shopify.com>
2013-09-24 14:37:09 -0400

    download isp and org databases in addition to city database

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,26 @@
-class geolitecity {
-  exec { "download database":
-    command => "sudo sh -c 'mkdir -p /usr/share/GeoIP && curl http://s3.amazonaws.com/burkelibbey/GeoIPCity.dat.gz | gunzip > /usr/share/GeoIP/GeoIPCity.dat'",
+class geolocation {
+  exec { "create geolocation directory":
+  	command => "sudo sh -c 'mkdir -p /usr/share/GeoIP'",
+  	onlyif => [
+  	  "test ! -d /usr/share/GeoIP"
+  	]
+  }
+  exec { "download city database":
+    command => "sudo sh -c 'curl http://s3.amazonaws.com/shopify/s/files/2aee18af95c47346ccf4ee7e315a5d62/GeoIPCity.dat.gz | gunzip > /usr/share/GeoIP/GeoIPCity.dat'",
     onlyif => [
       "test ! -s /usr/share/GeoIP/GeoIPCity.dat"
+    ]
+  }
+  exec { "download isp database":
+    command => "sudo sh -c 'curl http://s3.amazonaws.com/shopify/s/files/2aee18af95c47346ccf4ee7e315a5d62/GeoIPISP.dat.gz | gunzip > /usr/share/GeoIP/GeoIPISP.dat'",
+    onlyif => [
+      "test ! -s /usr/share/GeoIP/GeoIPISP.dat"
+    ]
+  }
+  exec { "download org database":
+    command => "sudo sh -c 'curl http://s3.amazonaws.com/shopify/s/files/2aee18af95c47346ccf4ee7e315a5d62/GeoIPOrg.dat.gz | gunzip > /usr/share/GeoIP/GeoIPOrg.dat'",
+    onlyif => [
+      "test ! -s /usr/share/GeoIP/GeoIPOrg.dat"
     ]
   }
 }
