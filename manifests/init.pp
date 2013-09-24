202e6de10e74f042a3bc5bb725a21fdce68cd786
Dave Isaacs <dave.isaacs@gmail.com>
2013-09-24 16:25:56 -0400

    Add ordering requirements

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,22 +5,25 @@ class geolocation {
   	  "test ! -d /usr/share/GeoIP"
   	]
   }
-  exec { "download org database":
-    command => "sudo sh -c 'curl http://s3.amazonaws.com/shopify/s/files/2aee18af95c47346ccf4ee7e315a5d62/GeoIPOrg.dat.gz | gunzip > /usr/share/GeoIP/GeoIPOrg.dat'",
-    onlyif => [
-      "test ! -s /usr/share/GeoIP/GeoIPOrg.dat"
-    ]
-  }
   exec { "download city database":
     command => "sudo sh -c 'curl http://s3.amazonaws.com/shopify/s/files/2aee18af95c47346ccf4ee7e315a5d62/GeoIPCity.dat.gz | gunzip > /usr/share/GeoIP/GeoIPCity.dat'",
     onlyif => [
       "test ! -s /usr/share/GeoIP/GeoIPCity.dat"
-    ]
+    ],
+    require => Exec["create geolocation directory"]
   }
   exec { "download isp database":
     command => "sudo sh -c 'curl http://s3.amazonaws.com/shopify/s/files/2aee18af95c47346ccf4ee7e315a5d62/GeoIPISP.dat.gz | gunzip > /usr/share/GeoIP/GeoIPISP.dat'",
     onlyif => [
       "test ! -s /usr/share/GeoIP/GeoIPISP.dat"
-    ]
+    ],
+    require => Exec["create geolocation directory"]
+  }
+  exec { "download org database":
+    command => "sudo sh -c 'curl http://s3.amazonaws.com/shopify/s/files/2aee18af95c47346ccf4ee7e315a5d62/GeoIPOrg.dat.gz | gunzip > /usr/share/GeoIP/GeoIPOrg.dat'",
+    onlyif => [
+      "test ! -s /usr/share/GeoIP/GeoIPOrg.dat"
+    ],
+    require => Exec["create geolocation directory"]
   }
 }
